b18c549444ed4621f9637cae269be7cbb46d681e
Junha Hwang <84448319+junnha@users.noreply.github.com>
2025-11-26 03:22:15 +0900

    Update behavior_cluster.py

--- behavior_cluster.py
+++ behavior_cluster.py
@@ -3,7 +3,6 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 
-# (3개 상태로 가정)
 kmeans = KMeans(n_clusters=3, random_state=42, n_init=10)
 df['cluster'] = kmeans.fit_predict(X_scaled)
 
@@ -11,7 +10,6 @@ cluster_means = df.groupby('cluster')['acc_magnitude'].mean().sort_values()
 print("클러스터별 평균 가속도:\n")
 print(cluster_means)
 
-#클러스터 번호를 매핑
 mapping = {cluster_means.index[0]: 'rest',
             cluster_means.index[1]: 'normal',
             cluster_means.index[2]: 'overactive'}
